ee945b4243f84fd6b9aaedb8167d1aaf26b2324d
sofiabykovskaya <sofi454545@gmail.com>
2024-10-15 16:24:44 +0300

    made a basic AlertDialog for saving a items to list

--- app/src/main/java/com/example/shoppinglistapp/ShoppingList.kt
+++ app/src/main/java/com/example/shoppinglistapp/ShoppingList.kt
@@ -2,11 +2,16 @@ package com.example.shoppinglistapp
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -22,16 +27,32 @@ data class ShoppingItem(val id:Int,
                         var quantity: Int,
                         var isEditing: Boolean = false)
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun  ShoppingListApp() {
     var shoppingItems by remember {
         mutableStateOf(listOf<ShoppingItem>())
     }
+
+    var showDialog by remember {
+        mutableStateOf(false)
+    }
+
+    var itemName by remember {
+        mutableStateOf("")
+    }
+
+    var itemQuantity by remember {
+        mutableStateOf("")
+    }
+
     Column(
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
     ) {
-        Button(onClick = {}, modifier = Modifier.align(Alignment.CenterHorizontally)) {
+        Button(onClick = {
+                         showDialog = true
+        }, modifier = Modifier.align(Alignment.CenterHorizontally)) {
             Text(text = "Add item")
         }
         LazyColumn(modifier = Modifier
@@ -41,5 +62,50 @@ fun  ShoppingListApp() {
 
             }
         }
+    }
+
+    if(showDialog) {
+        AlertDialog(onDismissRequest = { showDialog = false }, confirmButton = {
+                            Row(modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(8.dp), horizontalArrangement = Arrangement.SpaceBetween) {
+                        Button(onClick = {
+                            if(itemName.isNotBlank()) {
+                                val newItem = ShoppingItem(
+                                    id = shoppingItems.size + 1,
+                                    name = itemName,
+                                    quantity = itemQuantity.toInt(),
+                                )
+
+                                shoppingItems = shoppingItems + newItem
+                                showDialog = false
+                                itemName = ""
+                            }
+                        }) {
+                            Text(text = "Add")
+                        }
+                                Button(onClick = { showDialog = false }) {
+                                    Text(text = "Cancel")
+                                }
+                            }
+        },
+            title = { Text(text = "Add Shopping Item")},
+            text = {
+                Column {
+                    OutlinedTextField(value = itemName , onValueChange = {
+                        itemName = it
+                    }, singleLine = true, modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(8.dp)
+                    )
+                    OutlinedTextField(value = itemQuantity , onValueChange = {
+                        itemQuantity = it
+                    }, singleLine = true, modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(8.dp)
+                    )
+                }
+            }
+            )
     }
 }
